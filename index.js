8ad9bfd457c67d296eaa77f937ddd142138940d7
ricardoaguiar <barrios.dev@gmail.com>
2021-06-24 19:07:50 +0200

    completed tests

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
 const diceGame = (diceRolls) => {
-  return [0, 0];
+  let playerOneScore = 0;
+  let playerTwoScore = 0;
+  let isItPlayerOneTurn = true;
+  let turn = [];
+
+  diceRolls.forEach((roll) => {
+    if (turn.length < 2 || turn[0] === turn[1]) {
+      turn.push(roll);
+    }
+    if (turn.length === 2 && turn[0] !== turn[1]) {
+      // turn ends with two rolls
+      if (isItPlayerOneTurn) {
+        playerOneScore += turn[0] + turn[1];
+        turn = [];
+        isItPlayerOneTurn = false;
+      } else {
+        playerTwoScore += turn[0] + turn[1];
+        turn = [];
+        isItPlayerOneTurn = true;
+      }
+    } else if (turn.length === 3) {
+      // turn ends with three rolls
+      if (isItPlayerOneTurn) {
+        playerOneScore += turn[0] + turn[1] + turn[2];
+        turn = [];
+        isItPlayerOneTurn = false;
+      } else {
+        playerTwoScore += turn[0] + turn[1] + turn[2];
+        turn = [];
+        isItPlayerOneTurn = true;
+      }
+    }
+  });
+
+  return [playerOneScore, playerTwoScore];
 };
 
 export default diceGame;
--- index.test.js
+++ index.test.js
@@ -7,3 +7,19 @@ it('returns an array of two numbers', () => {
   expect(typeof scores[0]).toBe('number');
   expect(typeof scores[1]).toBe('number');
 });
+
+it('works with no doubles', () => {
+  expect(diceGame([1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6])).toStrictEqual([
+    21, 21,
+  ]);
+  expect(diceGame([2, 3, 6, 1, 4, 5, 1, 2])).toStrictEqual([14, 10]);
+});
+
+it('works with no doubles but has repeating numbers', () => {
+  expect(diceGame([1, 2, 2, 3, 3, 4, 5, 6])).toStrictEqual([10, 16]);
+});
+
+it('works with doubles', () => {
+  expect(diceGame([1, 1, 2, 3, 3, 4, 1, 2, 3, 4])).toStrictEqual([7, 17]);
+  expect(diceGame([1, 2, 2, 2, 3, 3, 4, 5, 6])).toStrictEqual([10, 18]);
+});
